63dba9d52eff3d88b5a9785f86152939914c85fc
chenyichao <chenyichao@mana.com>
2017-06-29 13:19:14 +0800

    bootstrap components

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,9 @@
 import '../style/index.scss'
 import $ from 'jquery'
 import '../js/bootstrap.js'
+
+//由于性能的原因，工具提示和弹出框的 data 编程接口（data api）是必须要手动初始化的。
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
